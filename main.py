6663746da2632829c59d4413da2ae5e955ecfa58
subanaya <subanaya.thirunavukkarasu@rbe.sk.ca>
2020-10-25 18:26:53 +0000

    added import statements

---
+++ main.py
@@ -0,0 +1,10 @@
+# Course: CS 30
+# Period: 1
+# Date Created: 20/10/19
+# Date Last Modified: 20/10/25
+# Name: Subanaya Thirunavukkarasu
+# Description: Create a map for your game using imports
+
+import characters
+import locations
+import actions
